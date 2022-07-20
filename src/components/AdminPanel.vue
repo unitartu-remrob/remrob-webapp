a32a4027edcb4085e0ba9e5f4f5ffda6ace7497e
Tsapu <chooky823@gmail.com>
2022-07-20 16:35:21 +0300

    Inventory user assignment

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -81,42 +81,13 @@ export default {
 		}
     },
     methods: { 
-<<<<<<< Updated upstream
-				// "id": inv.id,
-				// "robot_id": inv.robot_id,
-				// "slug": inv.slug,
-				// "title": f"Robotont-{inv.robot_id}",
-				// "status": inv.status,
-				// "project": inv.project,
-				// "vnc_uri": inv.vnc_uri
         getInventory: function() {
             axios.get(this.$store.state.baseURL + "/inventory", {headers: this.$store.state.header}).then((res) => {
                 this.inventory = res.data
-				this.inventory.forEach(({robot_id, slug, vnc_uri}) => {
-					this.connections[slug] = {
-						// Change to .env later
-						id: robot_id,
-						vnc_uri: `http://localhost${vnc_uri}`
-					}
-				})
-=======
-        getInventory: function() {
-            axios.get(this.$store.state.baseURL + "/inventory", {headers: this.$store.state.header}).then((res) => {
-                this.inventory = res.data
->>>>>>> Stashed changes
 				this.listContainers()
             })			
         },
 		listContainers: async function() {
-<<<<<<< Updated upstream
-			const calls = this.inventory.map(({ slug }) => {
-				return axios.get(`${this.$store.state.containerAPI}/inspect/${slug}`, {headers: this.$store.state.header})
-			});
-			const results = await Promise.allSettled(calls)
-			const data = this.inventory.map(({ slug }, index) => {
-				return {
-					id: slug,
-=======
 			const calls = []
 			this.inventory.forEach(({ slug }) => {
 				calls.push(axios.get(`${this.$store.state.containerAPI}/stats/${slug}`, {headers: {...this.$store.state.header, "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate"}}));
@@ -128,37 +99,16 @@ export default {
 					robot_id,
 					id: slug,
 					vnc_uri: `http://localhost${vnc_uri}`, // TODO: change to .env
->>>>>>> Stashed changes
 					data: (results[index].status === 'fulfilled') 
 						?  results[index].value.data
 						:  404
 				}
-<<<<<<< Updated upstream
-			})
-			console.log(data)
-			this.containerList = data
-        },
-		startContainer: function(c) {
-			// robot_id required to update DB entry on the backend, slug used to start container
-			const robot_id = this.connections[c.id].id
-			axios.post(`${this.$store.state.containerAPI}/start/${robot_id}`, {slug: c.id}, {headers: this.$store.state.header}).then((res) => {
-                const { path } = res.data
-				// Update the UI
-				this.connections[c.id].vnc_uri = `http://localhost${path}`;
-				this.listContainers()
-            })	
-		},
-		stopContainer: function(c) {
-			axios.post(`${this.$store.state.containerAPI}/stop/${c.id}`, {}, {headers: this.$store.state.header}).then((res) => {
-				console.log(`${c.id} stopped`)
-=======
 			}).sort( (a, b) => a.robot_id > b.robot_id );
 			console.log("data fetched")
 			this.containerList = data
         },
 		startContainer: function(id) {
 			axios.post(`${this.$store.state.containerAPI}/start/${id}`, {}, {headers: this.$store.state.header}).then((res) => {
->>>>>>> Stashed changes
 				this.listContainers()
             })	
 		},
@@ -167,14 +117,11 @@ export default {
 				console.log(`${id} stopped`)
 				this.listContainers()
             })	
-<<<<<<< Updated upstream
-=======
 		},
 		removeContainer: function(id) {
 			axios.post(`${this.$store.state.containerAPI}/remove/${id}`, {}, {headers: this.$store.state.header}).then((res) => {
 				this.listContainers()
             })
->>>>>>> Stashed changes
 		}
     },
     created() {
@@ -182,22 +129,11 @@ export default {
         this.getInventory()	
     },
 	mounted() {
-<<<<<<< Updated upstream
-		//this.pollInterval = setInterval(() => this.listContainers(), 5000)
-	},
-	// Only hook that triggered for interval clearance (?)
-	beforeRouteLeave(to, from, next) {
-		//clearInterval(this.pollInterval)
-		next()
-	}
-	
-=======
 		this.pollInterval = setInterval(() => this.getInventory(), 2500)
 	},
 	beforeDestroy() {  
         clearInterval(this.pollInterval);
     }
->>>>>>> Stashed changes
 }
 </script>
 
