e6d06f15b997c75a83756a6267ac18802d39a00a
rauno99 <rauno.polluaar@gmail.com>
2022-09-25 23:08:29 +0300

    admin name to booking db

--- models.py
+++ models.py
@@ -24,6 +24,7 @@ class Bookings(db.Model):
     project = db.Column('project', db.String(100))
     start_time = db.Column('start_time', db.String(100))
     end_time = db.Column('end_time', db.String(100))
+    admin = db.Column("admin", db.String(255))
 
 class Inventory(db.Model):
     __tablename__ = "inventory"
