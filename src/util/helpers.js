f2c5fd786fc6b04dad50a564ed63f2a58cdb3e9a
Tsapu <chooky823@gmail.com>
2022-11-17 16:45:27 +0200

    option to configure app TZ

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -16,7 +16,8 @@ const adjustForLocal = (d) => {
 	const localTime = d.getTime();
 	const localoffset = d.getTimezoneOffset() * 60000; // getTimezoneOffset is in minutes
 	const utc = localTime + localoffset; // get utc time
-	const adjusted = new Date(utc + (2*3600000)) // EST is +3 in summer, +2 in winter
+	const UTC_OFFSET = Math.floor(process.env.VUE_APP_UTC_OFFSET)
+	const adjusted = new Date(utc + (UTC_OFFSET*3600000)) // EST is +3 in summer, +2 in winter
 	return adjusted
 }
 
