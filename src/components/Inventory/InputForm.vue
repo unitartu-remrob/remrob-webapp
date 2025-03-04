5414969d68d94c5992d59aac444a4473fa8fddcf
Tsapu <chooky823@gmail.com>
2025-03-04 12:40:15 +0200

    simtainer management in Inventory

--- src/components/Inventory/InputForm.vue
+++ src/components/Inventory/InputForm.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="d-flex align-items-center">
-		<label for="workcell" class="mr-2 mb-0">Workcell:</label>
-		<b-form-input :disabled="!edit" v-model="inventoryItem.cell" type="number" id="workcell" class="w-25 d-inline mr-3"></b-form-input>
+
+		<label v-if="!isSimtainer" for="workcell" class="mr-2 mb-0">Workcell:</label>
+		<b-form-input v-if="!isSimtainer" :disabled="!edit" v-model="inventoryItem.cell" type="number" id="workcell" class="w-25 d-inline mr-3"></b-form-input>
+		
 		<!-- <label for="project" class="mr-2 mb-0">Project:</label>
 		<b-form-input :disabled="!edit" v-model="inventoryItem.project" id="project" class="w-25 d-inline mr-3"></b-form-input> -->
 		<b-form-checkbox :disabled="!edit" class="mr-4 ml-4"
 			switch
-			v-model="inventoryItem.status"
+			v-model="itemStatus"
 			/>
 		<div>
 			<b-button :disabled="edit" class="mr-2" type="button" @click="editField()">Edit</b-button>
@@ -18,25 +20,35 @@
 <script>
 
 export default {
-	props: ['inventoryItem'],
+	props: ['inventoryItem', 'isSimtainer'],
 	data: function() {
 		return {
-			edit: false
+			edit: false,
+			itemStatus: null,
 		}
+	},
+	computed: {
+
 	},
 	methods: {
 		editField: function() {
 			this.edit = true
 		},
 		updateField: function() {
+			const updatedStatus = this.isSimtainer ? { open_to_public: this.itemStatus } : { status: this.itemStatus }
 			this.$emit('update', {
-				...this.inventoryItem
+				...this.inventoryItem,
+				...updatedStatus
 			})
 			this.edit = false
 		}
 	},
 	created: function() {
-
+		if (this.isSimtainer) {
+			this.itemStatus = this.inventoryItem.open_to_public
+		} else {
+			this.itemStatus = this.inventoryItem.status
+		}
 	},
 }
 </script>
--- src/components/Inventory/Inventory.vue
+++ src/components/Inventory/Inventory.vue
@@ -1,28 +1,36 @@
 <template>
-    <b-container fluid class="pt-3 bg-white">
+    <b-container fluid class="pt-3 pl-4 bg-white">
         <b-alert :show="dismissCountDown" dismissible @dismissed="dismissCountDown=0" :variant="alertType">{{message}}</b-alert>
-        <b-modal ok-title="Confirm" @ok="deleteInventory(selectedForDelete)" title="Delete user" id="delete-modal">
+        <b-modal ok-title="Confirm" @ok="deleteInventory(selectedForDelete)" title="Delete inventory item" id="delete-modal">
             <h4>Are you sure you want to remove this inventory item?</h4>
         </b-modal>
         <b-row class="mt-3">
-            <b-col>
+            <b-col class="col-4">
                 <b-form>
-                    <b-form-group label="Robot nr.">
+                    <b-form-group label="Robot nr.:">
                         <b-form-input type="number" v-model="robotId">  
                         </b-form-input>
                     </b-form-group>
-                    <b-form-group label="Robot name">
+                    <b-form-group label="Robot name:">
                         <b-form-input type="string" v-model="robotLabel">  
                         </b-form-input>
                     </b-form-group>
-                    <b-button @click="createInventory">Submit</b-button>
+                    <b-button variant="success" @click="createInventory">Add robot</b-button>
                 </b-form>
             </b-col>
-            <b-col>
-                <h1 class="text-center panel-title">Robot inventory</h1>
+            <b-col class="col-6">
+                <h1 class="text-center panel-title">Inventory</h1>
             </b-col>
         </b-row>
         <br>
+        <b-row>
+            <b-col class="col-8">
+                <h2>Robots</h2>
+            </b-col>
+            <b-col>
+                <h2>Simtainers</h2>
+            </b-col>
+        </b-row>
         <b-row>
             <b-col>
                 <b-table striped :items="inventory" :fields="fields">
@@ -30,13 +38,21 @@
                         <b-button type="button" class="delete-button" variant="dark" @click="function() {selectedForDelete = slug; $bvModal.show('delete-modal')}">Remove</b-button>
                     </template>
                     <template #cell(project)="{ item }">
-                        <InventoryInput :inventoryItem="item" @update="updateInventory"/>
+                        <InventoryInput :inventoryItem="item" :isSimtainer=false @update="updateInventory"/>
                     </template>            
+                </b-table>
+            </b-col>
+            <b-col class="col-4 ml-5">
+                
+                <b-table :items="simtainerInventory" :fields="simtainerFields">
+                    <template #cell(open_to_public)="{ item }">
+                        <InventoryInput :inventoryItem="item" :isSimtainer=true @update="updateSimtainerInventory"/>
+                    </template>    
                 </b-table>
             </b-col>
         </b-row>
         <b-row>
-            <b-col class="d-flex justify-content-center mt-3 align-items-center">
+            <b-col class="d-flex justify-content-center align-items-center">
                 <b-img style="max-width: 20vw" :src="require('@/assets/field_plan.jpg')"></b-img>
                 <h4>⬅️ Cell legend relative to door</h4>
             </b-col>
@@ -53,6 +69,7 @@ export default {
             robotId: null,
             robotLabel: null,
             inventory: [],
+            simtainerInventory: [],
             formState: [],
             fields: [
                 { key: "title", label: "ID" },
@@ -61,6 +78,10 @@ export default {
                 { key: "project", label: "Change settings" },              
                 // { key: "status", label: "Available" },
                 { key: "delete", label: ""},
+            ],
+            simtainerFields: [
+                { key: "slug", label: "ID" },         
+                { key: "open_to_public", label: "Public access" },
             ],
             selectedForDelete: null,
             message: "",
@@ -77,14 +98,21 @@ export default {
     },
     methods: {
         getInventory: function() {
+            this.getRobotInventory()
+            this.getSimtainerInventory()
+        },
+        getRobotInventory: function() {
             this.$api.get(`/api/v1/inventory`).then((res) => {
                 this.inventory = res.data
-                console.log(this.inventory)
+            })
+        },
+        getSimtainerInventory: function() {
+            this.$api.get(`/api/v1/simtainers`).then((res) => {
+                this.simtainerInventory = res.data
             })
         },
         createInventory: function() {
             if (this.robotId === null) {
-                console.log("empty submit")
                 return 
             }
             const data = {
@@ -98,7 +126,7 @@ export default {
                 this.message = res.data;
                 this.alertType = "success";
                 this.dismissCountDown = this.dismissSec;
-                this.getInventory()
+                this.getRobotInventory()
             })
         },
         deleteInventory: function(id) {
@@ -120,7 +148,17 @@ export default {
                 this.alertType = "success";
                 this.dismissCountDown = this.dismissSec;
 			})
-		}
+		},
+        updateSimtainerInventory: function(item) {
+            const data = {
+                "open_to_public": item.open_to_public
+            }
+            this.$api.put(`/api/v1/simtainers/${item.slug}`, data).then((res) => {
+                this.message = res.data;
+                this.alertType = "success";
+                this.dismissCountDown = this.dismissSec;
+            })
+        }
     },
     created() {
         this.getInventory()
@@ -129,7 +167,6 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
 
 .panel-title {
     font-size: 2.8rem;
